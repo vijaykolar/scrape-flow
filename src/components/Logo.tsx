fc617fae8347e05d0402dc5b37eb397333833f09
vijaykolar <vijayikolar@gmail.com>
2025-02-13 21:12:13 +0530

    working on sidebar

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -19,7 +19,12 @@ export const Logo = ({ fontSize = "text-2xl", iconSize = 20 }: Props) => {
       <div className="rounded-xl bg-gradient-to-r from-emerald-500 to-emerald-600 p-2">
         <SquareDashedMousePointer size={iconSize} className="stroke-white" />
       </div>
-      Logo
+      <div>
+        <span className="bg-gradient-to-r from-emerald-500 to-emerald-600 bg-clip-text text-transparent">
+          Flow
+        </span>
+        <span className="text-stone-700 dark:text-stone-300">Scrape</span>
+      </div>
     </Link>
   );
 };
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   CoinsIcon,
   HomeIcon,
@@ -6,6 +7,9 @@ import {
   ShieldCheckIcon,
 } from "lucide-react";
 import { Logo } from "@/components/Logo";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import { usePathname } from "next/navigation";
 
 type Routes = {
   href: string;
@@ -37,10 +41,33 @@ const routes: Routes = [
 ];
 
 export const DesktopSidebar = () => {
+  const pathname: string = usePathname();
+  const activeRoute =
+    routes.find(
+      (route) => route.href.length > 0 && pathname.includes(route.href),
+    ) || routes[0];
   return (
     <div className="hidden md:block relative min-w-[280px] max-w-[280px] h-screen overflow-hidden w-full bg-primary/5 dark:bg-secondary/30 dark:text-foreground text-muted-foreground border-r-2 border-separate">
       <div className="flex items-center justify-center gap-2 border-b-[1px] border-separate p-4">
         <Logo />
+      </div>
+      <div className="p-2">TODO CREDITS</div>
+      <div className="flex flex-col p-2 gap-2">
+        {routes.map((route) => (
+          <Link
+            href={route.href}
+            key={route.href}
+            className={buttonVariants({
+              variant:
+                activeRoute.href === route.href
+                  ? "sidebarActiveItem"
+                  : "sidebarItem",
+            })}
+          >
+            <route.icon size={20} />
+            {route.label}
+          </Link>
+        ))}
       </div>
       Sidebar
     </div>
