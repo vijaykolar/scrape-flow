e9e68550d22449b6009b143a69bdbe42ab7e8973
vijaykolar <vijayikolar@gmail.com>
2025-02-16 09:21:33 +0530

    Enhance HomePage layout by adding a title and a demo button with icon

--- src/app/(dashboard)/(home)/page.tsx
+++ src/app/(dashboard)/(home)/page.tsx
@@ -1,9 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { Tv2Icon } from "lucide-react";
 
 export default function HomePage() {
   return (
     <div>
-      <Button>Okay w</Button>
+      <h1 className="text-5xl font-bold mb-2">Home page</h1>
+      <Button>
+        <Tv2Icon />
+        Watch demo
+      </Button>
     </div>
   );
 }
