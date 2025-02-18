8cdf85c630a399678cb3370d92abf2d92e4423c4
vijaykolar <vijayikolar@gmail.com>
2025-02-18 18:24:10 +0530

    Disable additional ESLint rules for flexibility.
    Disabled `@typescript-eslint/no-explicit-any` and `no-console` rules to allow the use of `any` types and console statements during development. This improves flexibility but may require caution to maintain code quality.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,8 @@ const eslintConfig = [
     rules: {
       "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "no-console": "off",
     },
   }),
 ];
